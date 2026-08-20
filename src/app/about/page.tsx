f4932dbf6328e365c0aa6a514e265bad060612ea
zaynedoc <zaynedockery@gmail.com>
2026-08-19 20:59:18 -0400

    Initialize

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+};
+
+export default function AboutPage() {
+  return <main aria-label="About" />;
+}
---
+++ src/app/expro/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Expro",
+};
+
+export default function ExproPage() {
+  return <main aria-label="Expro" />;
+}
